2f59c9f6cf11aba4ce8bb9c523358f5c4e4de655
joelisreal <joeligb123@gmail.com>
2024-03-02 12:38:27 -0800

    Updated docker

--- server/index.js
+++ server/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-const PORT = process.env.PORT || 8000;
+// const PORT = process.env.PORT || 8000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+// server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+server.listen(8000, () => {
+    console.log("server listening on port 8000");
+})
