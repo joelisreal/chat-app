8ef9dbc22c9b946d9dda6161fd3e5efbd86a7639
joelisreal <joeligb123@gmail.com>
2024-03-03 10:30:56 -0800

    Update file paths and add Chat and Join components

--- server/index.js
+++ server/index.js
@@ -4,11 +4,22 @@ const http = require('http');
 
 // const PORT = process.env.PORT || 8000;
 
+const router = require('./router');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
+io.on('connection', (socket) => {
+    console.log('We have a new connection!!!');
+
+    socket.on('disconnect', () => {
+        console.log('User had left!!!');
+    })
+});
+app.use(router);
 server.listen(8000, () => {
     console.log("server listening on port 8000");
 })
