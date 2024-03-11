d01ed28737c019c61b91af25cace7aacf1cf6cbf
joelisreal <joeligb123@gmail.com>
2024-03-11 05:51:26 -0700

    Add socket.io functionality to Chat component and server

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+let socket;
 
 const Chat = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:8000';   
+    // const location = useLocation(); 
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);//, {transports: ['websocket', 'polling', 'flashsocket']});
+        setName(name);
+        setRoom(room);
+        console.log(socket);
+        console.log(name, room);
+        console.log(location.search);
+        console.log(window.location.search);
+        socket.emit('join', { name, room }, ({error}) => {
+            alert(error);
+            console.log(error);
+        });
+    }, [ENDPOINT, location.search]);
     return (
         <h1>Chat</h1>
     );
--- server/index.js
+++ server/index.js
@@ -8,12 +8,27 @@ const router = require('./router');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+});
 
 // server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
 
 io.on('connection', (socket) => {
     console.log('We have a new connection!!!');
+
+    socket.on('join', ({ name, room }, callback) => {
+        console.log(name, room);
+
+        const error = true;
+
+        if (error) {
+            callback({ error: 'error' });
+        }
+    });
 
     socket.on('disconnect', () => {
         console.log('User had left!!!');
