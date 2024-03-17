4eef2f45278d1576c3a57b53682bcea2e12a5043
joelisreal <joeligb123@gmail.com>
2024-03-17 08:56:24 -0700

    Add chat functionality to Chat component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -8,6 +8,8 @@ let socket;
 const Chat = () => {
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
     const ENDPOINT = 'localhost:8000';   
     const location = useLocation(); 
 
@@ -32,8 +34,38 @@ const Chat = () => {
             socket.off();
         }
     }, [ENDPOINT, location.search]);
+
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        });
+        console.log(messages)
+        console.log(message)
+    }, [messages]
+    );
+
+    // function for sending messages
+    const sendMessage = (event) => {
+        event.preventDefault();
+        console.log(45)
+        console.log(message)
+        if (message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
+    console.log(message, messages)
+    
     return (
-        <h1>Chat</h1>
+        <div className="outerContainer">
+            <div className="container">
+                <input 
+                    value={message}
+                    onChange={(event) => setMessage(event.target.value)}
+                    onKeyDown={event => event.key === 'Enter' ? sendMessage(event) : null}
+                />
+            </div>
+        </div>
     );
 }
 
